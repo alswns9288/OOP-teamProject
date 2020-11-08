bbf34b70171ecda76dd57c349f26f50d595720f0
yoonsuh0615 <72903951+yoonsuh0615@users.noreply.github.com>
2020-11-08 18:06:32 +0900

    Update MyMouseListener.java

--- OOP_TeamProject/src/coronaMap/MyMouseListener.java
+++ OOP_TeamProject/src/coronaMap/MyMouseListener.java
@@ -9,5 +9,12 @@ public class MyMouseListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton button = (JButton)e.getSource();
+		if(e.getSource() == button) {
+			if(button.getText().equals("확진자 경로")) {
+				PrintpplGUI ppl = new PrintpplGUI();
+				ppl.createAndShowGUI();
+			}
+				
+		}
 	}
 }
