8ae61499b1a9f1209225c33b91adf02eef8c97b0
mir1325 <72898551+mir1325@users.noreply.github.com>
2020-11-10 01:38:09 +0900

    Add files via upload

--- OOP_TeamProject/src/GUI/LoginGUI.java
+++ OOP_TeamProject/src/GUI/LoginGUI.java
@@ -4,32 +4,16 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class LoginGUI extends JFrame {
+public class LoginGUI extends JPanel {
+	
+	JPanel background;
+	JButton signUpButton;
+	JButton loginButton;
 
-	private JPanel background;
-	private JTextField TextFieldUsername;
-	private JTextField TextFieldPassword;
-	private JButton signUpButton;
-	private JButton loginButton;
-
-	/*public static void main(String[] args) {
-		LoginGUI login = new LoginGUI();
-		login.creatAndShowGUI();
-	}*/
-
-	public void creatAndShowGUI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("로그인");
-		setPreferredSize(new Dimension(400, 300));
-		setLocation(500, 50);
-		background = new JPanel();
-		setContentPane(background);
-		background.setLayout(null);
-		setResizable(false);
-		
-		setMainGUI();
-		pack();
-		setVisible(true);
+	public LoginGUI() {
+		setLayout(null);
+		createLabel();
+		createTextField();
 		
 		signUpButton.addActionListener(new ActionListener() {		
 			@Override
@@ -43,41 +27,38 @@ public class LoginGUI extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(null, "로그인 되었습니다.");
-				dispose();
+				//dispose();
 			}
 		});
 	}
-
-	private void setMainGUI() {
-		createLabel();
-		createTextField();
-	}
-
-	private void createTextField() {
-		TextFieldUsername = new JTextField();
-		TextFieldUsername.setBounds(157, 52, 176, 35);
-		background.add(TextFieldUsername);
 		
-		TextFieldPassword = new JTextField();
-		TextFieldPassword.setBounds(157, 103, 176, 35);
-		background.add(TextFieldPassword);	
+	private void createTextField() {
+		JTextField Username = new JTextField();
+		Username.setLocation(150, 55);
+		Username.setSize(150, 30);
+		add(Username);
+	
+		JTextField Password = new JTextField();
+		Password.setLocation(150, 105);
+		Password.setSize(150, 30);
+		add(Password);	
 		
 		signUpButton = new JButton("회원가입");
 		signUpButton.setBounds(229, 154, 104, 29);
-		background.add(signUpButton);
+		add(signUpButton);
 		
 		loginButton = new JButton("로그인");
 		loginButton.setBounds(108, 154, 106, 29);
-		background.add(loginButton);	
+		add(loginButton);	
 	}
 
 	private void createLabel() {
 		JLabel LabelLogin = new JLabel("Username");
 		LabelLogin.setBounds(41, 52, 69, 35);
-		background.add(LabelLogin);
+		add(LabelLogin);
 		
 		JLabel LabelPassword = new JLabel("Password");
 		LabelPassword.setBounds(41, 103, 69, 35);
-		background.add(LabelPassword);	
+		add(LabelPassword);	
 	}	
 }
