553ce863e0b088422bf2b58f888cf39c723970f3
drago <drago@DESKTOP-R3HL0TD>
2020-11-12 15:52:44 +0900

    비교하는 화면에서 데이터를 여러개 받을수 있게 변경

--- OOP_TeamProject/src/GUI/PathCompareGUI.java
+++ OOP_TeamProject/src/GUI/PathCompareGUI.java
@@ -6,58 +6,97 @@ import javax.swing.border.Border;
 import javax.swing.table.DefaultTableModel;
 
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class PathCompareGUI extends JPanel {
-	int matchNum;
+	int dangerRate = 1;
 	String myName;
-	String myPlace;
-	// String myDay;
-	String myTime;
+	ArrayList<String> myPlace = new ArrayList<String>();
+	ArrayList<String> myDay = new ArrayList<String>();
+	ArrayList<String> myTime = new ArrayList<String>();
 
-//	public PathCompareGUI(String myName, String myPlace, /*String myDay, */ String myTime) {
-//		this.myName = myName;
-//		this.myPlace = myPlace;
-//		//this.myDay = myDay;
-//		this.myTime = myTime;
-//	}
+	public PathCompareGUI(String inputName, String[] inputPlace, 
+			String[] inputDay, String[] inputTime) { // 외부로부터 이름, 장소, 날짜, 시간을 배열 형태로 전달받음
+		myName = inputName;
+		for(int i = 0; i<inputPlace.length; i++) {
+			myPlace.add(inputPlace[i]);
+		}
+		for(int i = 0; i<inputDay.length; i++) {
+			myDay.add(inputDay[i]);
+		}
+		for(int i = 0; i<inputTime.length; i++) {
+			myTime.add(inputTime[i]);
+		}
+	}
 
 	public PathCompareGUI() {
 		setLayout(new BorderLayout());
-		setTop();
-		setTable();
+		setMyPath();
+		setPositivesPath();
 		setResult();
 	}
 
 	private void setResult() {
-		String result = matchNum + "contacts expectation, "; // "총 "+matchNum+"명 접촉 예상, 코로나 검사"
-		if (matchNum == 0) {
-			result += "No Exam Required, Keep Social Distancing"; // "불필요, 자가격리 후 경과를 지켜보세요"
-		} else {
-			result += "Exam Required, Visit Selective Care Center"; // "필요, 가까운 선별진료소를 방문하세요!!"
+		String result = "Danger Rate : "+dangerRate; // "위험도 "+dangerRate+" 코로나 검사"
+		switch (dangerRate) {
+		case 1:
+			result += " No Exam Required, No Match with Positive"; //"불필요, 확진자의 경로와 일치하는 부분이 없음"
+			break;
+		case 2:
+			result += " No Exam Required, Visit On Another Day"; //"불필요, 확진자의 경로와 장소가 일치하지만 다른 날에 방문"
+			break;
+		case 3:
+			result += " Exam Required, Match Place and Day"; //"필요, 확진자의 경로와 날짜, 장소가 일치!!"
+			break;
+		case 4:
+			result += " Exam Required, Match Multiple Place and Day"; //"필요, 확진자의 경로와 복수개의 날짜와 장소가 일치!!"
+			break;
+		default:
+			result += " Exam Required, Exactly Macth"; //"필요, 확진자의 경로와 날짜와 시간, 장소가 일치!!"
+			break;
 		}
 		JLabel resultLabel = new JLabel(result);
 		resultLabel.setPreferredSize(new Dimension(400, 40));
 		add(resultLabel, BorderLayout.PAGE_END);
 	}
 
-	private void setTop() {
-		String header[] = { "Me", "Place", /* "Day", */ "Time" }; // 아직 날짜는 데이터 파일에 없다., "나", "위치", /*"날짜",*/ "시간"
-		String contents[][] = { { myName, myPlace, /* myDay, */ myTime } };
+	private void setMyPath() {
+		String header[] = {"Me", "Place", "Day","Time"}; // "나", "위치", "날짜", "시간"
+		String contents[][] = new String[myPlace.size()][4];
+		for(int i = 0; i<myPlace.size(); i++) {
+			contents[i][0] = null;
+			contents[i][1] = myPlace.get(i);
+			contents[i][2] = myDay.get(i);
+			contents[i][3] = myTime.get(i);
+		}
+		contents[0][0] = myName;
+		
 		JTable myTable = new JTable(contents, header);
 		JScrollPane jscrollPane = new JScrollPane(myTable);
-		jscrollPane.setPreferredSize(new Dimension(400, 40));
+		jscrollPane.setPreferredSize(new Dimension(400, 300));
 		add(jscrollPane, BorderLayout.PAGE_START);
 	}
 
-	private void setTable() {
-		String header[] = { "No. of posttives", "Place", /* "Day", */ "Time" }; // 아직 날짜는 데이터 파일에 없다., "확진자 번호", "위치",
-																				// /*"날짜",*/ "시간"
+	private void setPositivesPath() {
+		String header[] = {"No. of postives", "Place", "Day", "Time"}; // "확진자 번호", "위치", "날짜", "시간"
+																
 		// 검색기능을 통해 content 데이터 구축
-		String contents[][] = { { "No1", "Demo Place1", "Demo Time1" }, // "번호1", "데모위치1", "데모시간1"
-				{ "No2", "Demo Place2", "Demo Time2" }, { "No3", "Demo Place3", "Demo Time3" } };
+		/*
+		PlaceManagement manage = new PlaceManagement();
+		int overlapNo = 0; //겹치는 경로의 수 
+		String contents[][] = new String[overlapNo][4];
+		for (int i = 0; i<overlapNo; i++) {
+			//내용 추가
+		}
+		*/
+		String contents[][] = {
+				{"No1", "Demo Place1", "Demo Day1", "Demo Time1"}, //"번호1", "데모위치1", "데모날짜1", "데모시간1"
+				{"No2", "Demo Place2", "Demo Day2", "Demo Time2"},
+				{"No3", "Demo Place3", "Demo Day3", "Demo Time3"}
+		};
 		JTable myTable = new JTable(contents, header);
 		JScrollPane jscrollPane = new JScrollPane(myTable);
-		jscrollPane.setPreferredSize(new Dimension(400, 600));
+		jscrollPane.setPreferredSize(new Dimension(400, 300));
 		add(jscrollPane, BorderLayout.CENTER);
 	}
 }
